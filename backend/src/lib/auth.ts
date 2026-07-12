a4dcdcc461d552f50c22cdda91193debcab64d36
ozan-fn <me@ozan.my.id>
2026-07-12 12:50:39 +0700

    feat(auth): disable origin check in betterauth

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -9,6 +9,9 @@ const prisma = new PrismaClient();
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_BASE_URL || "http://localhost:5173",
+  advanced: {
+    disableOriginCheck: true
+  },
   database: prismaAdapter(prisma, {
     provider: "mongodb",
   }),
